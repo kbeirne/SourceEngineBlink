72793902b2f51bbf9d1b8e26ce9111e5f4408565
Kevin Beirne <kmbeirne@gmail.com>
2012-11-06 16:58:25 +0000

    Source Engine Code with movement bob added

---
+++ Blink/run_mod.bat
@@ -0,0 +1,1 @@
+@"c:\program files (x86)\steam\steamapps\nevik_eeirnb\source sdk base 2007\hl2.exe" -dev -game "c:\program files (x86)\steam\steamapps\SourceMods\blink001" -allowdebug %1 %2 %3 %4 %5 %6 %7 %8 %9
